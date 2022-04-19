e3887e9c06b1dfafa8a12539154b568aecc3ed0b
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-19 19:12:26 -0400

    unit test with Storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
+
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+   } else if (props.danger) {
+      buttonClass += " button--danger";
+   }
+   return <button className={buttonClass} disabled={props.disabled} onClick={props.onClick}>{props.children }</button>;
 }
