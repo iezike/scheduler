b6036377e8d8fa0fb692d4f1ed3d0f8f9ce85f3d
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-20 19:30:05 -0400

    create InterviewerList and the storybook

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,18 +2,6 @@ import React from "react";
 import "./InterviewerListItem.scss";
 import classNames from "classnames";
 
-// export default function InterviewerListItem() {
-//   return (
-//     <li className="interviewers__item">
-//       <img
-//         className="interviewers__item-image"
-//         src="https://i.imgur.com/LpaY82x.png"
-//         alt="Sylvia Palmer"
-//       />
-//       Sylvia Palmer
-//     </li>
-//   );
-// }
 
 export default function InterviewerListItem(props) {
   const interviewerStyles = classNames("interviewers__item", {
@@ -23,12 +11,8 @@ export default function InterviewerListItem(props) {
     "interviewers__item--selected-image": props.selected,
   })
   return (
-    <li className={interviewerStyles} onClick={props.setInterviewer}>
-      <img
-        className={imageStyles}
-        src={props.avatar}
-        alt={props.name}
-      />
+    <li className={interviewerStyles} onClick={() => props.setInterviewer(props.id)}>
+      <img className={imageStyles} src={props.avatar} alt={props.name} />
       {props.selected && props.name}
     </li>
   )
