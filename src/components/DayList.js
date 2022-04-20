0a78399c01003a3daa09620638e09c8e06cc7a14
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-20 09:45:53 -0400

    render DayList to Application

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList() {
+export default function DayList(props) {
   const daylist = props.days.map(dayObj => {
     return (
       <DayListItem
