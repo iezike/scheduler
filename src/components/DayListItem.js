6ad2da76cf95e6e35e4618479e04ac8400e2cf4d
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-20 00:16:20 -0400

    style and test DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,28 @@
+import React from "react";
+import "./DayListItem.scss";
+import classNames from "classnames";
+
+
+
 export default function DayListItem(props) {
+  const formatSpots = spots => {
+    if (!spots) {
+      return `no spots remaining`;
+    }
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    }
+    return `${spots} spots remaining`
+  }
+
+  let dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots
+  })
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="day-list_item">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
