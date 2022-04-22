827a3f7494545edf3dfff3e0fd9e69cce734c033
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-22 03:05:18 -0400

    populate the Appointment component with schedule appointments from mock data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import InterviewList from "./InterviewerList";
+import Appointment from "./Appointment";
+
+// some mock data
 const days = [
   {
     id: 1,
@@ -20,6 +23,46 @@ const days = [
     spots: 0,
   },
 ];
+
+//Some mock data 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
@@ -45,7 +88,13 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
+      <section className="schedule" >
+        {Object.values(appointments).map(appointment =>
+          <Appointment
+            key={appointment.id}
+            {...appointment}
+          />)}
+        <Appointment key="last" time="5pm" />
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
