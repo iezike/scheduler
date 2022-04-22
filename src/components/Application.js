f11436ef21ddd63f450fe85fe5ae45f5eef7beca
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-22 19:50:31 -0400

    Create an effect to make an api request using axios and replace the days with resulting data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import "components/Application.scss";
@@ -6,24 +6,7 @@ import DayList from "./DayList";
 import InterviewList from "./InterviewerList";
 import Appointment from "./Appointment";
 
-// some mock data
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
 
 //Some mock data 
 const appointments = {
@@ -67,6 +50,14 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
+  const [days, setDays] = useState([])
+  
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then((response) => {
+      setDays(response.data)
+    })
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
