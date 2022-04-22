4b7cc7121c25db5ada2ad957a3e0fb0af9cdae25
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-21 22:51:52 -0400

    Write Appointment Booked story

--- stories/index.js
+++ stories/index.js
@@ -203,5 +203,15 @@ storiesOf("Appointment", module) // stories for Appointment (index) component an
       <Appointment time="5pm" />
     </Fragment>
   ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
   
 
