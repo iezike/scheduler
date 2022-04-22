528f2cd630f577d00cc1d7cd1454f7897a91fbdc
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-21 23:25:50 -0400

    Add mock data

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,11 +5,51 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 
+// Mock data
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
     </article>
   );
 }
