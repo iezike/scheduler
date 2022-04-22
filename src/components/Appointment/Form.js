64ae8dfe947ae1a407915368f1b76d02ee03729e
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-21 20:13:14 -0400

    add props and states to the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,24 +1,42 @@
-import React from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
+import React, { useState } from 'react';
 
-export default function Form() {
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  //helper function to clear form fields
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name={props.name}
             type="text"
             placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           /*
             This must be a controlled component
             your code goes here
           */
           />
         </form>
-
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={(event) => setInterviewer(event)}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
