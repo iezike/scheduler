874787c29d9d2bb0202700270a22aecd7889c5ee
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-23 22:08:41 -0400

    use map function to retrive data as schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+//import dependency files
 import "components/Application.scss";
 import DayList from "./DayList";
 import InterviewList from "./InterviewerList";
@@ -8,6 +9,7 @@ import Appointment from "./Appointment";
 import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
+  //define state variables
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -16,17 +18,30 @@ export default function Application(props) {
   })
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const schedule = state.appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   const setDay = day => setState({ ...state, day });
-
+  //useEffect as a hook
   useEffect(() => {
+    // multiple api calls using axios promise
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments')
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
     ])
     .then((all) => {
-      console.log(all[0], all[1])
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data}));
+      //multiple state variables update
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
   }, []);
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,9 +3,6 @@ export function getAppointmentsForDay(state, day) {
   //From state.days filter appointment day where day name matches the day parameter 
   const days = state.days.find(d => d.name === day) // days is an object
   if (days) {
-    //  print day to view
-    console.log(days)
-
     appointments = days.appointments.map(id => state.appointments[id])
   }
   // From the days.appointmeents retrive the state appointment for each id 
