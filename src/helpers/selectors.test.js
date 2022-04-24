fe064eb69629e6197c4d2ff7063e91ed914b7b13
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-23 22:15:13 -0400

    add interviewers mock data for unit testing

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -27,6 +27,18 @@ const state = {
       time: "4pm",
       interview: { student: "Chad Takahashi", interviewer: 2 }
     }
+  },
+  interviewers: {
+    "1": {  
+      "id": 1,
+      "name": "Sylvia Palmer",
+      "avatar": "https://i.imgur.com/LpaY82x.png"
+    },
+    "2": {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    }
   }
 };
 
