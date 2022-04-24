309aa20bb606e6bcd97dad886ba8dfc3d893bb7a
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-23 21:04:26 -0400

    Use Promise.all to make multiple api calls and update state variables at a time using useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,48 +5,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import InterviewList from "./InterviewerList";
 import Appointment from "./Appointment";
-
-
-
-//Some mock data 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -54,14 +13,21 @@ export default function Application(props) {
     days: [],
     appointments: {}
   })
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState({ ...state, days });
-  
+
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days").then((response) => {
-      setDays(response.data)
-    })
-  }, [])
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments')
+    ])
+    .then((all) => {
+      console.log(all[0], all[1])
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data}));
+    });
+  }, []);
 
   return (
     <main className="layout">
@@ -86,7 +52,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule" >
-        {Object.values(appointments).map(appointment =>
+        {dailyAppointments.map(appointment =>
           <Appointment
             key={appointment.id}
             {...appointment}
