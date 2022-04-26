ea1b5a930c34221a7eb57ef6b8678be4bc348098
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-25 20:15:43 -0400

    create save function

--- src/components/Application.js
+++ src/components/Application.js
@@ -30,6 +30,13 @@ export default function Application(props) {
   function bookInterview(id, interview) {
     console.log(id, interview);
   }
+  
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+  }
 
   const dailyAppointments = getAppointmentsForDay(state, state.day); // get daily appointments as an array
   const schedule = dailyAppointments.map((appointment) => {
@@ -55,10 +62,10 @@ export default function Application(props) {
       axios.get('http://localhost:8001/api/appointments'),
       axios.get('http://localhost:8001/api/interviewers')
     ])
-    .then((all) => {
-      //multiple state variables update
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    });
+      .then((all) => {
+        //multiple state variables update
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      });
   }, []);
 
   return (
