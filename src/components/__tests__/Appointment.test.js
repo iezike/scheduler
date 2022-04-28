d640f6944042a36d31220c9447615afdf065276c
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-04-28 12:52:00 -0400

    create the cypress.json file and set up

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -14,9 +14,12 @@ import { render } from "@testing-library/react";
 */
 import Application from "components/Application";
 
-/*
-  A test that renders a React Component
-*/
-it("renders without crashing", () => {
-  render(<Application />);
+
+describe("Appointment", () => {
+  /*
+    A test that renders a React Component
+  */
+  it("renders without crashing", () => {
+    render(<Application />);
+  });
 });
